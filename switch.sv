f5a1e703d53dbdd97e1df0eee3f1c131134f1c3b
MadmanSonic <82250920+MadmanSonic@users.noreply.github.com>
2022-04-22 01:45:50 -0700

    Update switch.sv

--- switch.sv
+++ switch.sv
@@ -1,8 +1,7 @@
 `timescale 1ns / 1ps
-
 import SystemVerilogCSP::*;
 
-module switch(Channel I, Channel O1, Channel O2, Channel O3, Channel O4, Channel O5);
+module switch(interface packet_in, interface O1, interface O2, interface O3, interface O4, interface O5);//O1 Y up, O2 Y down, O3 X right, O4 X left, O5 to PE  
     parameter WIDTH = 33;
     parameter FL = 2;
     parameter BL = 1;
@@ -11,87 +10,43 @@ module switch(Channel I, Channel O1, Channel O2, Channel O3, Channel O4, Channel
     logic [1:0] Xoffset, Yoffset;
     
     parameter in_type = 3'b0;
-    parameter address_0 = 0;    //y
-    parameter address_1 = 0;    //y
-    parameter address_2 = 0;    //x
-    parameter address_3 = 0;    //x
+    parameter address_X = 2'b0;    //y
+    parameter address_Y = 2'b0;    //y
     
     logic [3:0] address;
       
     always begin
-        address = {address_3, address_2, address_1, address_0};
+        address = {address_X, address_Y};
         // Packet arrives to the switch
-        I.Receive(packet);
+        packet_in.Receive(packet);
         #FL;
         
-        Xoffset = packet [WIDTH-2: WIDTH-3] - address[3:2];
-        Yoffset = packet [WIDTH-4: WIDTH-5] - address[1:0];
+        //Xoffset = packet [31: 30] - address[3:2];
+        //Yoffset = packet [29: 28] - address[1:0];
         
-        if(in_type == 3'b0) //from North
-        begin
-            if ((Xoffset == 0) && (Yoffset == 0))
-                O5.Send(packet);    // packet reaches the destination
-            else if (Yoffset < 0)
-                O3.Send(packet);    //heading to the south
-            else if (Xoffset > 0)
-                O2.Send(packet);    //heading to the east
-            else if (Xoffset < 0)
-                O1.Send(packet);    //heading to the west
-        end
-        
-        else if (in_type == 3'b001) //from East
-        begin
-            if ((Xoffset == 0) && (Yoffset == 0))
-                O5.Send(packet);
-            else if (Yoffset > 0)
-                O4.Send(packet);    //heading to the north
-            else if (Yoffset < 0)
-                O3.Send(packet);    //heading to the south
-            else if (Xoffset < 0)
-                O1.Send(packet);    //heading to the west
-        end
-        
-        else if (in_type == 3'b010) //From South
-        begin
-            if ((Xoffset == 0) && (Yoffset == 0))
-                O5.Send(packet);
-           else if (Yoffset > 0)
-                O4.Send(packet);    //heading to the north
-            else if (Xoffset > 0)
-                O2.Send(packet);    //heading to the east
-            else if (Xoffset < 0)
-                O1.Send(packet);    //heading to the west
-        end
-        
-        else if (in_type == 3'b011) // From West
-        begin
-            if ((Xoffset == 0) && (Yoffset == 0))
-                O5.Send(packet);
-           else if (Yoffset > 0)
-                O4.Send(packet);    //heading to the north
-            else if (Yoffset < 0)
-                O3.Send(packet);    //heading to the south
-            else if (Xoffset > 0)
-                O2.Send(packet);    //heading to the east
-        end
-        
-       else if (in_type == 3'b100)
-        begin
-            if ((Xoffset == 0) && (Yoffset == 0))
-                O5.Send(packet);
-           else if (Yoffset > 0)
-                O4.Send(packet);    //heading to the north
-            else if (Yoffset < 0)
-                O3.Send(packet);    //heading to the south
-            else if (Xoffset > 0)
-                O2.Send(packet);    //heading to the east
-            else if (Xoffset < 0)
-                O1.Send(packet);    //heading to the west
-        end
-        
+       if (packet [29: 28] > address[1:0])
+            begin
+            O1.Send(packet);
+            end
+        else if (packet [29: 28] < address[1:0])
+                begin
+                O2.Send(packet);
+                end
+        else if (packet [29: 28] == address[1:0])
+                begin
+                if(packet [31: 30] > address[3:2])
+                    begin
+                        O3.Send(packet);
+                    end
+                else if(packet [31: 30] < address[3:2])
+                    begin
+                        O4.Send(packet);
+                    end
+                else if(packet [31: 30] == address[3:2])
+                    begin
+                        O5.Send(packet);
+                    end
+                end
         #BL;
     end
 endmodule
-
-
-

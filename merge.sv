bf5d1c0546bec100541ed201f23f5e4bd9fcf33d
MadmanSonic <82250920+MadmanSonic@users.noreply.github.com>
2022-04-22 01:45:10 -0700

    Update merge.sv

--- merge.sv
+++ merge.sv
@@ -1,13 +1,11 @@
 `timescale 1ns / 1ps
-
 import SystemVerilogCSP::*;
 
-module merge(Channel A, Channel B, Channel S, Channel O);
-    
+module merge(Channel A, Channel B, Channel S, Channel O);    
     parameter WIDTH = 33;
+    parameter FL = 2;
+    parameter BL = 1;
     logic sel;
-    parameter FL = 4;
-    parameter BL = 6;
     logic [WIDTH-1:0] data, out;
       
     always begin
@@ -16,13 +14,12 @@ module merge(Channel A, Channel B, Channel S, Channel O);
             begin          
                 A.Receive(data);
              end
-        else
+        else if (sel == 1)
             begin
                 B.Receive(data);           
             end
         #FL;   
         O.Send(data);
-        #BL;
-                                    
+        #BL;                                    
     end
 endmodule

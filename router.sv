7cf8bb2951625de294d7a059c11742904e5b90c1
MadmanSonic <82250920+MadmanSonic@users.noreply.github.com>
2022-04-22 01:51:48 -0700

    Update router.sv

--- router.sv
+++ router.sv
@@ -1,34 +1,32 @@
-`timescale 1ns / 1ps
-
+`timescale 1ns/100ps
 import SystemVerilogCSP::*;
 
-module router(Channel N_in, Channel E_in, Channel S_in, Channel W_in, Channel PE_in, Channel N_out, Channel E_out, Channel S_out, Channel W_out, Channel PE_out);
+module router (interface in_left,interface in_right,interface in_up,interface in_down,interface in_pe,interface out_left,interface out_right,interface out_up,interface out_down,interface out_pe );
 
-parameter FL = 2;
-parameter BL = 1;
-parameter WIDTH = 33;
-parameter address_0 = 0;
-parameter address_1 = 0;
-parameter address_2 = 0;
-parameter address_3 = 0;
+parameter FL=2;
+parameter BL=1;
+parameter WIDTH=33;
+parameter address_X=2'b00;
+parameter address_Y=2'b00;
 
-    
-    Channel #(.hsProtocol(P4PhaseBD), .WIDTH(WIDTH)) CH[19:0] ();
-    
-    //switch(I, O1, O2, O3, O4, O5)
-    switch #(.WIDTH(WIDTH), .FL(FL), .BL(BL), .address_0(address_0), .address_1(address_1), .address_2(address_2), .address_3(address_3), .in_type(000)) N_switch(.I(N_in), .O1(CH[0]), .O2(CH[1]), .O3(CH[2]), .O4(CH[3]));
-    switch #(.WIDTH(WIDTH), .FL(FL), .BL(BL), .address_0(address_0), .address_1(address_1), .address_2(address_2), .address3(address_3), .in_type(001)) E_switch(.I(E_in), .O1(CH[4]), .O2(CH[5]), .O3(CH[6]), .O4(CH[7]));
-    switch #(.WIDTH(WIDTH), .FL(FL), .BL(BL), .address_0(address_0), .address_1(address_1), .address_2(address_2), .address3(address_3), .in_type(010)) S_switch(.I(S_in), .O1(CH[8]), .O2(CH[9]), .O3(CH[10]), .O4(CH[11]));
-    switch #(.WIDTH(WIDTH), .FL(FL), .BL(BL), .address_0(address_0), .address_1(address_1), .address_2(address_2), .address3(address_3), .in_type(011)) W_switch(.I(W_in), .O1(CH[12]), .O2(CH[13]), .O3(CH[14]), .O4(CH[15]));
-    switch #(.WIDTH(WIDTH), .FL(FL), .BL(BL), .address_0(address_0), .address_1(address_1), .address_2(address_2), .address3(address_3), .in_type(100)) PE_switch(.I(PE_in), .O1(CH[16]), .O2(CH[17]), .O3(CH[18]), .O4(CH[19]));
+Channel #(.hsProtocol(P4PhaseBD), .WIDTH(WIDTH)) intf  [24:0] (); 
 
-    //arbiter_merge_four(A,B,C,D,O)
-    arbiter_merge_four #(.WIDTH(WIDTH), .FL(FL), .BL(BL)) N_abmg(.A(CH[4]), .B(CH[8]), .C(CH[12]), .D(CH[16]), .O(N_out));
-    arbiter_merge_four #(.WIDTH(WIDTH), .FL(FL), .BL(BL)) E_abmg(.A(CH[0]), .B(CH[9]), .C(CH[13]), .D(CH[17]), .O(E_out));
-    arbiter_merge_four #(.WIDTH(WIDTH), .FL(FL), .BL(BL)) S_abmg(.A(CH[1]), .B(CH[5]), .C(CH[14]), .D(CH[18]), .O(S_out));
-    arbiter_merge_four #(.WIDTH(WIDTH), .FL(FL), .BL(BL)) W_abmg(.A(CH[3]), .B(CH[7]), .C(CH[11]), .D(CH[19]), .O(W_out));
-    arbiter_merge_four #(.WIDTH(WIDTH), .FL(FL), .BL(BL)) PE_abmg(.A(CH[2]), .B(CH[6]), .C(CH[10]), .D(CH[15]), .O(PE_out));
-    
-    
+switch  #(.address_X(address_X), .address_Y(address_Y)) S_up(.packet_in(in_up), .O2(intf[0]), .O3(intf[1]), .O4(intf[2]), .O5(intf[3]), .O1(intf[20]));
+arbiter_merge_four  am_up(.A(intf[12]), .B(intf[4]), .C(intf[16]), .D(intf[8]), .O(out_up));
+
+switch  #(.address_X(address_X), .address_Y(address_Y)) S_down(.packet_in(in_down), .O1(intf[4]), .O3(intf[5]), .O4(intf[6]), .O5(intf[7]), .O2(intf[21]));
+arbiter_merge_four  am_down(.A(intf[17]), .B(intf[9]), .C(intf[0]), .D(intf[13]), .O(out_down));
+
+switch  #(.address_X(address_X), .address_Y(address_Y)) S_left(.packet_in(in_left), .O1(intf[8]), .O2(intf[9]), .O3(intf[10]), .O5(intf[11]), .O4(intf[22]));
+arbiter_merge_four  am_left(.A(intf[2]), .B(intf[14]), .C(intf[6]), .D(intf[19]), .O(out_left));
+
+switch  #(.address_X(address_X), .address_Y(address_Y)) S_right(.packet_in(in_right), .O1(intf[12]), .O2(intf[13]), .O4(intf[14]), .O5(intf[15]), .O3(intf[23]));
+arbiter_merge_four  am_right(.A(intf[5]), .B(intf[18]), .C(intf[10]), .D(intf[1]), .O(out_right));
+
+switch  #(.address_X(address_X), .address_Y(address_Y)) S_PE(.packet_in(in_pe), .O1(intf[16]), .O2(intf[17]), .O3(intf[18]), .O4(intf[19]), .O5(intf[24]));
+arbiter_merge_four  am_pe(.A(intf[11]), .B(intf[3]), .C(intf[15]), .D(intf[7]), .O(out_pe));
+
+//arbiter_merge_four(interface A, interface B, interface C, interface D, interface O);//ABCD count from left to right
+//switch(Channel packet_in, Channel O1, Channel O2, Channel O3, Channel O4, Channel O5);//O1 Y up, O2 Y down, O3 X right, O4 X left, O5 to PE 
+
 endmodule
-

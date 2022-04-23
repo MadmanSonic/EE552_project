2637c9f7c04f3dbd3237d608d7c54af73c4f2f38
MadmanSonic <82250920+MadmanSonic@users.noreply.github.com>
2022-04-22 23:58:28 -0700

    Update Mem_wrapper.sv

--- Mem_wrapper.sv
+++ Mem_wrapper.sv
@@ -26,7 +26,7 @@ parameter WIDTH = 8;
   logic [WIDTH-1:0] byteval[8:0];
   logic spikeval[24:0];
   logic [24:0] filter_packet;
-  logic [9:0] ifmap_packet;
+  //logic [9:0] ifmap_packet;
   logic [32:0] packet;
   logic [32:0] rec_packet;
   logic [WIDTH-1 : 0] mem_potentials [2:0] [2:0];
@@ -62,7 +62,7 @@ parameter WIDTH = 8;
 				fromMemGetData.Receive(spikeval[i*5 + j]);
 				$display("%m received ifm[%d][%d] = %b",i,j,spikeval[i*5 + j]);				
 				// do processing (delete this line)
-				#simulating_processing_delay;
+				//#simulating_processing_delay;
 			end // ify
 			#mem_delay; // wait for them to arrive
 		end // ifx
@@ -71,85 +71,86 @@ parameter WIDTH = 8;
 		//Send data to NOC
 		for (int i = 0; i < num_filts_x; i++) begin
 			//for (int j = 0; j < num_filts_y; ++j) begin
-			filter_packet = {0, byteval[i*3 + 2], byteval[i*3 + 1], byteval[i*3 + 0]};
-			for (int j = 0; j < 9; j++) begin
-					if (i == 0) begin
-							ifmap_packet = {1, spikeval[(j+2)*5 + 2], spikeval[(j+2)*5 + 1], spikeval[(j+2)*5], spikeval[(j+1)*5 + 2], spikeval[(j+1)*5 + 1], spikeval[(j+1)*5], spikeval[j + 2], spikeval[j + 1],spikeval[j]};
-							case (j) 
+			filter_packet = {1'b0, byteval[i*3 + 2], byteval[i*3 + 1], byteval[i*3 + 0]};
+			for (int j = 0; j < 9; j++) 
+			begin
+					if (i == 0) 
+					   begin
+						case (j) 
 								0 : begin
 									packet = {filter_packet[24], 4'b0010, 4'b1101, filter_packet[23:0]};
 									toNOC.Send(packet);
-									#BL;
-									packet = {ifmap_packet[9], 4'b0010, 4'b1101, ifmap_packet[23:0]};
+									packet = {1'b1, 4'b0010, 4'b1101, 15'b000000000000000, spikeval[12], spikeval[11], spikeval[10], spikeval[7], spikeval[6], spikeval[5], spikeval[2], spikeval[1],spikeval[0]};
 									toNOC.Send(packet);
+									$display("j = %d", j);
 									#BL;
 									end
 								1 : begin
 									packet = {filter_packet[24], 4'b0110, 4'b1101, filter_packet[23:0]};	
 									toNOC.Send(packet);
-									#BL;
-									packet = {ifmap_packet[9], 4'b0110, 4'b1101, ifmap_packet[23:0]};
+									packet = {1'b1, 4'b0110, 4'b1101, 15'b000000000000000, spikeval[13], spikeval[12], spikeval[11], spikeval[8], spikeval[7], spikeval[6], spikeval[3], spikeval[2],spikeval[1]};
 									toNOC.Send(packet);
+									$display("j = %d", j);
 									#BL;
 									end	
 								2 : begin
 									packet = {filter_packet[24], 4'b1010, 4'b1101, filter_packet[23:0]};
 									toNOC.Send(packet);
-									#BL;
-									packet = {ifmap_packet[9], 4'b1010, 4'b1101, ifmap_packet[23:0]};
+									packet = {1'b1, 4'b1010, 4'b1101, 15'b000000000000000, spikeval[14], spikeval[13], spikeval[12], spikeval[9], spikeval[8], spikeval[7], spikeval[4], spikeval[3],spikeval[2]};
 									toNOC.Send(packet);
+									$display("j = %d", j);
 									#BL;									
 									end	
 								3 : begin
 									packet = {filter_packet[24], 4'b0001, 4'b1101, filter_packet[23:0]};
 									toNOC.Send(packet);
-									#BL;
-									packet = {ifmap_packet[9], 4'b0001, 4'b1101, ifmap_packet[23:0]};
+									packet = {1'b1, 4'b0001, 4'b1101, 15'b000000000000000, spikeval[17], spikeval[16], spikeval[15], spikeval[12], spikeval[11], spikeval[10], spikeval[7], spikeval[6],spikeval[5]};
 									toNOC.Send(packet);
+									$display("j = %d", j);
 									#BL;																		
 									end	
 								4 : begin
 									packet = {filter_packet[24], 4'b0101, 4'b1101, filter_packet[23:0]};	
 									toNOC.Send(packet);
-									#BL;
-									packet = {ifmap_packet[9], 4'b0101, 4'b1101, ifmap_packet[23:0]};
+									packet = {1'b1, 4'b0101, 4'b1101, 15'b000000000000000, spikeval[18], spikeval[17], spikeval[16], spikeval[13], spikeval[12], spikeval[11], spikeval[8], spikeval[7],spikeval[6]};
 									toNOC.Send(packet);
+									$display("j = %d", j);
 									#BL;				
 									end	
 								5 : begin
 									packet = {filter_packet[24], 4'b1001, 4'b1101, filter_packet[23:0]};
 									toNOC.Send(packet);
-									#BL;
-									packet = {ifmap_packet[9], 4'b1001, 4'b1101, ifmap_packet[23:0]};
+									packet = {1'b1, 4'b1001, 4'b1101, 15'b000000000000000, spikeval[19], spikeval[18], spikeval[17], spikeval[14], spikeval[13], spikeval[12], spikeval[9], spikeval[8],spikeval[7]};
 									toNOC.Send(packet);
+									$display("j = %d", j);
 									#BL;											
 									end	
 								6 : begin
 									packet = {filter_packet[24], 4'b0000, 4'b1101, filter_packet[23:0]};	
 									toNOC.Send(packet);
-									#BL;
-									packet = {ifmap_packet[9], 4'b0000, 4'b1101, ifmap_packet[23:0]};
+									packet = {1'b1, 4'b0000, 4'b1101, 15'b000000000000000, spikeval[22], spikeval[21], spikeval[20], spikeval[17], spikeval[16], spikeval[15], spikeval[12], spikeval[11],spikeval[10]};
 									toNOC.Send(packet);
+									$display("j = %d", j);
 									#BL;		
 									end	
 								7 : begin
 									packet = {filter_packet[24], 4'b0100, 4'b1101, filter_packet[23:0]};	
 									toNOC.Send(packet);
-									#BL;
-									packet = {ifmap_packet[9], 4'b0100, 4'b1101, ifmap_packet[23:0]};
+									packet = {1'b1, 4'b0100, 4'b1101, 15'b000000000000000, spikeval[23], spikeval[22], spikeval[21], spikeval[18], spikeval[17], spikeval[16], spikeval[13], spikeval[12],spikeval[11]};
 									toNOC.Send(packet);
+									$display("j = %d", j);
 									#BL;		
 									end	
 								8 : begin
 									packet = {filter_packet[24], 4'b1000, 4'b1101, filter_packet[23:0]};	
 									toNOC.Send(packet);
-									#BL;
-									packet = {ifmap_packet[9], 4'b1000, 4'b1101, ifmap_packet[23:0]};
+									packet = {1'b1, 4'b1000, 4'b1101, 15'b000000000000000, spikeval[24], spikeval[23], spikeval[22], spikeval[19], spikeval[18], spikeval[17], spikeval[14], spikeval[13],spikeval[12]};
 									toNOC.Send(packet);
+									$display("j = %d", j);
 									#BL;		
 									end	
 							endcase
-						end//if
+					   end//if
 					else 
 						begin
 						case (j) 
@@ -201,86 +202,73 @@ parameter WIDTH = 8;
 							endcase
 						end
 			end
-			
+		end	
 		
 		//receive data from DPKT
-		for (int i = 0; i < 9; i++) begin
+        for (int i = 0; i < 9; i++) 
+		begin
 			fromNOC.Receive(rec_packet);
 			#FL;
 			case(rec_packet[27:24]) 
 				4'b0010 : 	begin	
 							j = 0;
 							k = 0;
-							mem_potentials[j][k] =  rec_packet[7:0];
-							mem_spikes[j][k] = rec_packet[8];
+							mem_spikes[j][k] = rec_packet[0];
 							end
 				4'b0110 : 	begin	
 							j = 0;
 							k = 1;
-							mem_potentials[j][k] =  rec_packet[7:0];
-							mem_spikes[j][k] = rec_packet[8];
+							mem_spikes[j][k] = rec_packet[0];
 							end
 				4'b1010 : 	begin	
 							j = 0;
 							k = 2;
-							mem_potentials[j][k] =  rec_packet[7:0];
-							mem_spikes[j][k] = rec_packet[8];
+							mem_spikes[j][k] = rec_packet[0];
 							end
 				4'b0001 : 	begin	
 							j = 1;
 							k = 0;
-							mem_potentials[j][k] =  rec_packet[7:0];
-							mem_spikes[j][k] = rec_packet[8];
+							mem_spikes[j][k] = rec_packet[0];
 							end
 				4'b0101 : 	begin	
 							j = 1;
 							k = 1;
-							mem_potentials[j][k] =  rec_packet[7:0];
-							mem_spikes[j][k] = rec_packet[8];
+							mem_spikes[j][k] = rec_packet[0];
 							end
 				4'b1001 : 	begin	
 							j = 1;
 							k = 2;
-							mem_potentials[j][k] =  rec_packet[7:0];
-							mem_spikes[j][k] = rec_packet[8];
+							mem_spikes[j][k] = rec_packet[0];
 							end
 				4'b0000 : 	begin	
 							j = 2;
 							k = 0;
-							mem_potentials[j][k] =  rec_packet[7:0];
-							mem_spikes[j][k] = rec_packet[8];
+							mem_spikes[j][k] = rec_packet[0];
 							end			
 				4'b0100 : 	begin	
 							j = 2;
 							k = 1;
-							mem_potentials[j][k] =  rec_packet[7:0];
-							mem_spikes[j][k] = rec_packet[8];
+							mem_spikes[j][k] = rec_packet[0];
 							end		
 				4'b1000 : 	begin	
 							j = 2;
 							k = 2;
-							mem_potentials[j][k] =  rec_packet[7:0];
-							mem_spikes[j][k] = rec_packet[8];
+							mem_spikes[j][k] = rec_packet[0];
 							end				
 			endcase
-		end	
-		
+		end			
 		
 		// write back membrane potentials & spikes
 		// TO DO: you need to get them from the NoC first!
 		for (int i = 0; i < ofx; i++) begin
-			for (int j = 0; j < ofy; j++) begin							
-				toMemWrite.Send(write_mempots);
-				toMemX.Send(i);
-				toMemY.Send(j);
-				toMemSendData.Send(mem_potentials[i][j]);
-				
+			for (int j = 0; j < ofy; j++) begin											
 				toMemWrite.Send(write_ofmaps);
 				toMemX.Send(i);
 				toMemY.Send(j);				
 				toMemSendData.Send(mem_spikes[i][j]);
 			end // ofy
 		end // ofx
+		
 		$display("%m sent all output spikes and stored membrane potentials for timestep t = %d at time = %d",t,$time);
 		toMemT.Send(t);
 		$display("%m send request to advance to next timestep at time t = %d",$time);
@@ -288,8 +276,7 @@ parameter WIDTH = 8;
 	$display("%m done");
 	#mem_delay; // let memory display comparison of golden vs your outputs
 	$stop;
-  end
-  end
+  end//initial
   
   always begin
 	#200;
